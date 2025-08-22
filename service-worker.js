5543cc78a56c320c9edfaa2b382289017b272b05
carbaviana-arch <carbaviana@gmail.com>
2025-08-22 18:50:12 +0200

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,31 @@
-const CACHE_NAME = "registro-horas-v1";
+const CACHE_NAME = "registro-horas-v1.1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+  "./index.html?v=1.1",
+  "./style.css?v=1.1",
+  "./script.js?v=1.1",
+  "./manifest.json?v=1.1",
+  "./icon-192.png?v=1.1",
+  "./icon-512.png?v=1.1"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
 });
